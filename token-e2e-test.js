f8512d74aa5528e1afcb824c1b554ef676e17415
Prapti <praptishrestha@gmail.com>
2021-01-21 13:48:01 -0800

    Update token-e2e-test.js
    Added block comment to the top of the file.

--- token-e2e-test.js
+++ token-e2e-test.js
@@ -1,6 +1,12 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+// ***************************************************************
+// - [#] indicates a test step (e.g. # Go to a page)
+// - [*] indicates an assertion (e.g. * Check the title)
+// - Use element ID when selecting an element. Create one if none.
+// ***************************************************************
+
 import {getRandomId} from '../utils/index';
 
 import {getAdminAccount} from '../support/env';
